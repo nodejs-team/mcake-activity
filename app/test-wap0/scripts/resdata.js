5cb825fc15b825850e6ad24dfae9f63c7b0efc2b
shimilygood <1356300819@qq.com>
2017-02-08 15:59:49 +0800

    add test-wap0

---
+++ app/test-wap0/scripts/resdata.js
@@ -0,0 +1,12 @@
+var resData = [
+    {
+        "name":"a_jpg",
+        "type":"image",
+        "url":"a.jpg"
+    },
+    {
+        "name":"b_png",
+        "type":"image",
+        "url":"b.png"
+    }
+];
