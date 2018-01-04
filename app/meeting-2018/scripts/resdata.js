230d3dda017230dde1d26140454385ebb25f1b96
shimilygood <1356300819@qq.com>
2018-01-04 14:30:08 +0800

    meeting-2018

--- app/meeting-2018/scripts/resdata.js
+++ app/meeting-2018/scripts/resdata.js
@@ -1,197 +1,52 @@
 var resData = [
     {
-        "name":"vip-bg_png",
+        "name":"meet-bg_jpg",
         "type":"image",
-        "url":"vip-bg.png"
+        "url":"meet-bg.jpg"
     },
     {
-        "name":"bangshuli_png",
+        "name":"WeChat_png",
         "type":"image",
-        "url":"bangshuli.png"
+        "url":"WeChat.png"
     },
     {
-        "name":"btnbg_png",
+        "name":"icon-1_png",
         "type":"image",
-        "url":"btnbg.png"
+        "url":"icon-1.png"
     },
     {
-        "name":"cake-1_png",
+        "name":"icon-2_png",
         "type":"image",
-        "url":"cake-1.png"
+        "url":"icon-2.png"
     },
     {
-        "name":"cake-2_png",
+        "name":"icon-3_png",
         "type":"image",
-        "url":"cake-2.png"
+        "url":"icon-3.png"
     },
     {
-        "name":"cake-3_png",
+        "name":"icon-4_png",
         "type":"image",
-        "url":"cake-3.png"
+        "url":"icon-4.png"
     },
     {
-        "name":"cake-4_png",
+        "name":"meet-banner_jpg",
         "type":"image",
-        "url":"cake-4.png"
+        "url":"meet-banner.jpg"
     },
     {
-        "name":"cakebg_png",
+        "name":"meet-banner2_jpg",
         "type":"image",
-        "url":"cakebg.png"
+        "url":"meet-banner2.jpg"
     },
     {
-        "name":"go-btn_png",
+        "name":"meet-banner3_jpg",
         "type":"image",
-        "url":"go-btn.png"
+        "url":"meet-banner3.jpg"
     },
     {
-        "name":"img-1_png",
+        "name":"meet-banner4_jpg",
         "type":"image",
-        "url":"img-1.png"
-    },
-    {
-        "name":"img-2_png",
-        "type":"image",
-        "url":"img-2.png"
-    },
-    {
-        "name":"img-3_png",
-        "type":"image",
-        "url":"img-3.png"
-    },
-    {
-        "name":"img-4_png",
-        "type":"image",
-        "url":"img-4.png"
-    },
-    {
-        "name":"input_png",
-        "type":"image",
-        "url":"input.png"
-    },
-    {
-        "name":"m-c1_png",
-        "type":"image",
-        "url":"m-c1.png"
-    },
-    {
-        "name":"m-c2_png",
-        "type":"image",
-        "url":"m-c2.png"
-    },
-    {
-        "name":"m-c3_png",
-        "type":"image",
-        "url":"m-c3.png"
-    },
-    {
-        "name":"m-c4_png",
-        "type":"image",
-        "url":"m-c4.png"
-    },
-    {
-        "name":"minus_png",
-        "type":"image",
-        "url":"minus.png"
-    },
-    {
-        "name":"more_png",
-        "type":"image",
-        "url":"more.png"
-    },
-    {
-        "name":"m-text_png",
-        "type":"image",
-        "url":"m-text.png"
-    },
-    {
-        "name":"next_png",
-        "type":"image",
-        "url":"next.png"
-    },
-    {
-        "name":"plus_png",
-        "type":"image",
-        "url":"plus.png"
-    },
-    {
-        "name":"pr-bd_png",
-        "type":"image",
-        "url":"pr-bd.png"
-    },
-    {
-        "name":"prev_png",
-        "type":"image",
-        "url":"prev.png"
-    },
-    {
-        "name":"price-1_png",
-        "type":"image",
-        "url":"price-1.png"
-    },
-    {
-        "name":"price-2_png",
-        "type":"image",
-        "url":"price-2.png"
-    },
-    {
-        "name":"price-3_png",
-        "type":"image",
-        "url":"price-3.png"
-    },
-    {
-        "name":"price-4_png",
-        "type":"image",
-        "url":"price-4.png"
-    },
-    {
-        "name":"quan_png",
-        "type":"image",
-        "url":"quan.png"
-    },
-    {
-        "name":"select_png",
-        "type":"image",
-        "url":"select.png"
-    },
-    {
-        "name":"select-on_png",
-        "type":"image",
-        "url":"select-on.png"
-    },
-    {
-        "name":"sele-li_png",
-        "type":"image",
-        "url":"sele-li.png"
-    },
-    {
-        "name":"title_png",
-        "type":"image",
-        "url":"title.png"
-    },
-    {
-        "name":"title-1_png",
-        "type":"image",
-        "url":"title-1.png"
-    },
-    {
-        "name":"title-2_png",
-        "type":"image",
-        "url":"title-2.png"
-    },
-    {
-        "name":"title-3_png",
-        "type":"image",
-        "url":"title-3.png"
-    },
-    {
-        "name":"title-4_png",
-        "type":"image",
-        "url":"title-4.png"
-    },
-    {
-        "name":"vip_png",
-        "type":"image",
-        "url":"vip.png"
+        "url":"meet-banner4.jpg"
     }
 ];
